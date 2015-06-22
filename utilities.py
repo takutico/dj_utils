f2456d80b769a2fc0972f7d73e00a5e00ae19d9b
typ <takutico@users.noreply.github.com>
2015-06-22 14:07:18 +0800

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -38,3 +38,48 @@ def find_between( s, first, last ):
         return s[start:end]
     except ValueError:
         return ""
+
+
+def createThumbnail(user):
+    """ Create a thumbnail of image and save it as user image """
+    from io import StringIO
+    from PIL import Image
+    IMAGE_WIDTH = 300
+    IMAGE_HEIGHT = 300
+    try:
+        if not user.image:
+            return
+
+        image = Image.open(user.image.file).convert('RGB')
+        # If the image is smaller than 300 x 300, don't bother creating a thumbnail
+        width, height = image.size
+        if width < IMAGE_WIDTH and height < IMAGE_HEIGHT:
+            return
+
+        # Crop as little as possible to square, keeping the center.
+        if width > height:
+            aspect = height / float(width)
+            width = IMAGE_WIDTH
+            height = int(width * aspect)
+        else:
+            aspect = width / float(height)
+            height = IMAGE_HEIGHT
+            width = int(height * aspect)
+
+        # overwrite image file
+        resized = image.resize((width, height), Image.ANTIALIAS)
+        try:
+            save_path = user.image.path
+            ar = save_path.split('.')
+            if len(ar) == 2:
+                save_path = ar[0] + 's.' + ar[1]
+            resized.save(save_path, quality=90)
+            user.image = save_path.replace(settings.MEDIA_ROOT + '/', '')
+            user.save()
+        except Exception as e:
+            print("image save error")
+            print(str(e))
+        return
+    except Exception as e:
+        print(str(e))
+        return
